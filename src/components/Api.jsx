db6b022bc37ad29d38dcc486ad9972bf29ea8684
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-13 20:26:24 +0200

    imported React, useState and useEffect inside my component file and then inside my function I destructured the use state to manage change in apiPost and error. Fetched the api inside my useEffect which I used a function containing a try,catch method also incorporating async. I then created a list of items by mapping through the array of object we get from the apiPost new state.

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,9 +1,38 @@
+import React from "react"
+import { useState, useEffect } from "react"
 function Api () {
 
+    const [apiPost, setApiPost] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try{
+                const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+                if(!res.ok){
+                    throw new Error('Data fetching failed')
+                }
+                const data = await res.json()
+                setApiPost(data);
+            } catch (err){
+                setError(err.message);
+            }
+        };
+
+        fetchPosts();
+    }, []);
 
     return (
         <>
-        
+            <h1>Posts</h1>
+            <ul>
+                {apiPost.map( post => (
+                    <li>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
