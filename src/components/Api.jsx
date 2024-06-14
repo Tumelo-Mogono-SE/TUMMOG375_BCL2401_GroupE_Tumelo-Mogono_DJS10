53888d31cd8a4ee7477a524608435819c1f6b01a
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-14 14:45:05 +0200

    Created a new useState for loading which will be used to set the state for loading to be used in my fetchPost function. I then created two conditional statements to check for loading and one for error which if true will render information. I also added a key to have an id for each list element.

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -4,9 +4,11 @@ function Api () {
 
     const [apiPost, setApiPost] = useState([]);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const fetchPosts = async () => {
+            setLoading(true)
             try{
                 const res = await fetch('https://jsonplaceholder.typicode.com/posts');
                 if(!res.ok){
@@ -16,19 +18,34 @@ function Api () {
                 setApiPost(data);
             } catch (err){
                 setError(err.message);
+            } finally{
+                setLoading(false);
             }
         };
 
         fetchPosts();
     }, []);
 
+    if (loading) {
+        return (
+            <h1 className="loading">
+                Loading<span>.</span><span>.</span><span>.</span>
+            </h1>
+        )
+    }
+    if (error) {
+        return <h1>Data fetching failed</h1>
+    }
+
     return (
         <>
             <h1>Posts</h1>
             <ul>
                 {apiPost.map( (post, index) => (
-                    <li>
-                        <h2>{index + 1}. {post.title}</h2>
+                    <li key={post.id}>
+                        <h2>
+                            {index + 1}. {post.title}
+                        </h2>
                         <p>{post.body}</p>
                     </li>
                 ))}
